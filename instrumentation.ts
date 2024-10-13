8be00d0c10ce7a3cec7a4bd44f606615a37a9e98
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2024-10-13 14:08:53 +0600

    test

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,12 +1,14 @@
 import * as Sentry from '@sentry/nextjs';
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./sentry.server.config');
-  }
-
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    await import('./sentry.edge.config');
+  try {
+    if (process.env.NEXT_RUNTIME === 'nodejs') {
+      await import('./sentry.server.config');
+    } else if (process.env.NEXT_RUNTIME === 'edge') {
+      await import('./sentry.edge.config');
+    }
+  } catch (error) {
+    console.error('Error loading Sentry configuration:', error);
   }
 }
 
