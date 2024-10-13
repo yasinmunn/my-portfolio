ad150745c94a39cefb71694e06ff07a03c6d6d02
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2024-10-14 00:54:16 +0600

    remove sentry

--- components/ui/3d-pin.tsx
+++ components/ui/3d-pin.tsx
@@ -64,14 +64,14 @@ export const PinPerspective = ({
   href,
 }: {
   title?: string;
-  href?: string;
+  href?: string ;
 }) => {
   return (
     <motion.div className="pointer-events-none  w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
       <div className=" w-full h-full -mt-7 flex-none  inset-0">
         <div className="absolute top-0 inset-x-0  flex justify-center">
           <Link
-            href={href}
+            href={href || ""}
             target={"_blank"}
             className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
           >
